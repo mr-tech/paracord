37b98ea54194ea877586b3dd8948420288ce8b3f
Lando <calrissian@opayq.com>
2020-04-05 03:36:49 +0000

    Add Utils to export

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   Gateway: require('./src/clients/Gateway/Gateway'),
   Api: require('./src/clients/Api/Api'),
   Paracord: require('./src/clients/Paracord/Paracord'),
+  ParacordUtils: require('./src/utils'),
   ShardLauncher: require('./src/clients/Paracord/ShardLauncher'),
   Server: require('./src/rpc/server/Server'),
   constants: require('./src/constants'),
