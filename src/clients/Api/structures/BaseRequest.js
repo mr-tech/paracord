ab16ee096111d030ad383782fe65fe602c58c187
Lando <calrissian@opayq.com>
2020-04-06 09:34:20 +0000

    - Fix and extend rate limit route support - Add id property to gateway - Change shard property on get to return identity.shard - Outfit emitted gateway events with gateway shard id - Fix shard id assignment to guilds - Fix undefined member voicestate caching bug - Remove redundant code from member caching - Remove presence and user updates de-dup - Fix message event member caching bugs - Reduce redundant Paracord gateway shard creation - Altered fetchMember to return response object (with member embedded on 200) - Added fetchUser help function

--- src/clients/Api/structures/BaseRequest.js
+++ src/clients/Api/structures/BaseRequest.js
@@ -74,71 +74,95 @@ module.exports = class BaseRequest {
     else if (method === 'PATCH') key.push('u');
     else if (method === 'DELETE') key.push('d');
 
-    key.concat(rateLimitMinorParameters);
 
     // Below is a (incomplete) micro-optimization
-    // for (const param of rateLimitMinorParameters) {
-    // switch (param) {
-    //   case 'channels':
-    //     key.push('c');
-    //     break;
-    //   case 'members':
-    //     key.push('m');
-    //     break;
-    //   case 'guilds':
-    //     key.push('g');
-    //     break;
-    //   case 'messages':
-    //     key.push('msg');
-    //     break;
-    //   case 'roles':
-    //     key.push('r');
-    //     break;
-    //   case 'reactions':
-    //     key.push('re');
-    //     break;
-    //   case 'permissions':
-    //     key.push('p');
-    //     break;
-    //   case 'invites':
-    //     key.push('i');
-    //     break;
-    //   case 'emojis':
-    //     key.push('e');
-    //     break;
-    //   case 'embed':
-    //     key.push('em');
-    //     break;
-    //   case 'bans':
-    //     key.push('b');
-    //     break;
-    //   case 'prune':
-    //     key.push('pru');
-    //     break;
-    //   case 'bulk-delete':
-    //     key.push('bd');
-    //     break;
-    //   case 'typing':
-    //     key.push('t');
-    //     break;
-    //   case 'pins':
-    //     key.push('pins');
-    //     break;
-    //   case 'recipients':
-    //     key.push('re');
-    //     break;
-    //   case 'preview':
-    //     key.push('pre');
-    //     break;
-    //   case 'integrations':
-    //     key.push('int');
-    //     break;
-    //   case 'regions':
-    //     key.push('reg');
-    //     break;
-    //   default:
-    // }
-    // }
+    rateLimitMinorParameters.forEach((param) => {
+      switch (param) {
+        case 'channels':
+          key.push('c');
+          break;
+        case 'members':
+          key.push('m');
+          break;
+        case 'guilds':
+          key.push('g');
+          break;
+        case 'messages':
+          key.push('msg');
+          break;
+        case 'roles':
+          key.push('r');
+          break;
+        case 'webhooks':
+          key.push('w');
+          break;
+        case 'reactions':
+          key.push('re');
+          break;
+        case 'permissions':
+          key.push('p');
+          break;
+        case 'invites':
+          key.push('i');
+          break;
+        case 'users':
+          key.push('u');
+          break;
+        case 'emojis':
+          key.push('e');
+          break;
+        case 'embed':
+          key.push('em');
+          break;
+        case 'bans':
+          key.push('b');
+          break;
+        case 'prune':
+          key.push('pru');
+          break;
+        case 'bulk-delete':
+          key.push('bd');
+          break;
+        case 'typing':
+          key.push('t');
+          break;
+        case 'pins':
+          key.push('pins');
+          break;
+        case 'recipients':
+          key.push('re');
+          break;
+        case 'preview':
+          key.push('pre');
+          break;
+        case 'integrations':
+          key.push('int');
+          break;
+        case 'regions':
+          key.push('reg');
+          break;
+        case 'slack':
+          key.push('slack');
+          break;
+        case 'github':
+          key.push('github');
+          break;
+        case 'widget.png':
+          key.push('wid');
+          break;
+        case 'vanity-url':
+          key.push('vu');
+          break;
+        case 'sync':
+          key.push('s');
+          break;
+        case 'connections':
+          key.push('con');
+          break;
+        default:
+      }
+    });
+
 
     return key.join('-');
   }
