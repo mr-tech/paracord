64d133fc0c99044591bfad0f9734a8e5a32d4104
lando <calrissian@opayq.com>
2020-05-31 15:40:16 +0000

    fix type importing

--- dist/src/types/guild.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SystemChannelFlags = void 0;
-var SystemChannelFlags;
-(function (SystemChannelFlags) {
-    SystemChannelFlags[SystemChannelFlags["SUPPRESS_JOIN_NOTIFICATIONS"] = 1] = "SUPPRESS_JOIN_NOTIFICATIONS";
-    SystemChannelFlags[SystemChannelFlags["SUPPRESS_PREMIUM_SUBSCRIPTIONS"] = 2] = "SUPPRESS_PREMIUM_SUBSCRIPTIONS";
-})(SystemChannelFlags = exports.SystemChannelFlags || (exports.SystemChannelFlags = {}));
