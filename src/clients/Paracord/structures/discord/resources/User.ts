e4cc67b1b8fd908cf2cfb3b4bc10648593eaaa60
lando <calrissian@opayq.com>
2020-10-09 19:27:29 +0000

    - add more properties to user structure

--- src/clients/Paracord/structures/discord/resources/User.ts
+++ src/clients/Paracord/structures/discord/resources/User.ts
@@ -1,26 +1,62 @@
 import { RawUser, Snowflake } from '../../../../../types';
 import { FilteredProps } from '../../../types';
+import { timestampFromSnowflake } from '../../../../../utils';
 // import Resource from '../../Resource';
 import Presence from './Presence';
 
 // export default class User extends Resource<User, RawUser> {
 export default class User {
-  id: Snowflake;
+  /** the user's id */
+  #id: Snowflake; // identify
 
-  public username: string | undefined;
+  /** the user's username, not unique across the platform */
+  public username: string | undefined; // identify
 
-  public discriminator: string | undefined;
+  /** the user's 4-digit discord-tag */
+  public discriminator: string | undefined; // identify
 
-  public avatar: string | null | undefined;
+  /** the user's avatar hash */
+  public avatar: string | null| undefined; // identify
+
+  /** whether the user belongs to an OAuth2 application */
+  public bot: boolean | undefined; // identify
+
+  /** whether the user is an Official Discord System user (part of the urgent message system) */
+  public system: boolean | undefined; // identify
+
+  /** whether the user has two factor enabled on their account */
+  public mfaEnabled: boolean | undefined; // identify
+
+  /** the user's chosen language option */
+  public locale: string | undefined; // identify
+
+  /** whether the email on this account has been verified */
+  public verified: boolean | undefined; // email
+
+  /** the user's email */
+  public email: string | null | undefined; // email
+
+  /** the flags on a user's account */
+  public flags: number | undefined; // identify
+
+  /** the type of Nitro subscription on a user's account */
+  public premiumType: number | undefined; // identify
+
+  /** the public flags on a user's account */
+  public publicFlags: number | undefined; // identify
 
   public presence: Presence | undefined;
 
   #filteredProps: FilteredProps<User, RawUser> | undefined;
 
+  #lastAccessed: number;
+
   public constructor(filteredProps: FilteredProps<User, RawUser> | undefined, user: RawUser) {
-    // super(filteredProps, user.id);
     this.#filteredProps = filteredProps;
-    this.id = user.id;
+    this.#id = user.id;
+
+    const now = new Date().getTime();
+    this.#lastAccessed = now;
 
     if (filteredProps !== undefined) {
       this.initializeProperties(filteredProps);
@@ -29,8 +65,25 @@ export default class User {
     this.update(user);
   }
 
+  /** The epoch timestamp of when this guild was created extract from its Id. */
+  public get createdOn(): number | undefined {
+    return timestampFromSnowflake(this.#id);
+  }
+
+  public get id(): Snowflake {
+    return this.#id;
+  }
+
+  public get lastAccessed(): number {
+    return this.#lastAccessed;
+  }
+
   public get tag(): string | undefined {
     return this.username !== undefined && this.discriminator !== undefined ? `${this.username}#${this.discriminator}` : undefined;
+  }
+
+  public refreshLastAccessed(): void {
+    this.#lastAccessed = new Date().getTime();
   }
 
   public update(arg: RawUser): this {
@@ -49,6 +102,18 @@ export default class User {
         && (this.#filteredProps === undefined || 'avatar' in this)
         && arg.avatar !== this.avatar
     ) this.avatar = arg.avatar;
+    if (
+      arg.bot !== undefined
+        && (this.#filteredProps === undefined || 'bot' in this)
+        && arg.bot !== this.bot
+    ) this.bot = arg.bot;
+    if (
+      arg.system !== undefined
+        && (this.#filteredProps === undefined || 'system' in this)
+        && arg.system !== this.system
+    ) this.system = arg.system;
+
+    // TODO: add remaining props
 
     return this;
   }
